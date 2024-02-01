15f40f622fef8a459116b7ba106ced155ab6318e
vishnu <kannasri113@gmail.com>
2024-02-02 00:57:27 +0530

    first

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+var about = document.getElementById("About")
+var projects = document.getElementById("Projects")
+var resume = document.getElementById("Resume")
+function Resume(){
+   about.style.display = "none"
+   projects.style.display = "none"
+   resume.style.display = "block"
+}
+function Projects(){
+   about.style.display = "none"
+   resume.style.display = "none"
+   projects.style.display = "block"
+}

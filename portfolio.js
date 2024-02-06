e4c1866a1abf22c174dd146b284a6534d0b41b54
vishnu <kannasri113@gmail.com>
2024-02-06 23:00:04 +0530

    updates

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,24 @@
-// var about = document.getElementById("About")
-// var projects = document.getElementById("Projects")
-// var resume = document.getElementById("Resume")
-// function Resume(){
-//    about.style.display = "none"
-//    projects.style.display = "none"
-//    resume.style.display = "block"
-// }
-// function Projects(){
-//    about.style.display = "none"
-//    resume.style.display = "none"
-//    projects.style.display = "block"
-// }
+// $(document).ready(function(){
+//    $('.navbar-nav .nav-link').on('click', function(){
+//       $('.navbar-collapse').collapse('hide');
+//    });
+//  });
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
